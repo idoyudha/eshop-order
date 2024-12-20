c1fb486c629f7f14c14a864c8767e60bcb88b3e1
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-20 21:19:32 +0700

    feat: order set status

--- internal/entity/order.go
+++ internal/entity/order.go
@@ -6,6 +6,11 @@ import (
 	"github.com/google/uuid"
 )
 
+const (
+	ON_DELIVERY = "ON_DELIVERY"
+	REJECTED    = "REJECTED"
+)
+
 type Order struct {
 	ID         uuid.UUID
 	UserID     uuid.UUID
@@ -15,4 +20,14 @@ type Order struct {
 	CreatedAt  time.Time
 	UpdatedAt  time.Time
 	DeletedAt  time.Time
+}
+
+// admin accept payment and set the order status to ON_DELIVERY
+func (o *Order) SetStatusToOnDelivery() {
+	o.Status = ON_DELIVERY
+}
+
+// admin reject payment and set the order status to REJECTED
+func (o *Order) SetStatusToRejected() {
+	o.Status = REJECTED
 }
