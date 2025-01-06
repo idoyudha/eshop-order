89d4a38dd2c00cea54ba9492fa427a0612399feb
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-06 20:36:32 +0700

    fix: comment updateOrderStatus

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -26,7 +26,7 @@ type (
 
 	OrderCommand interface {
 		CreateOrder(context.Context, *entity.Order, string) error
-		UpdateOrderStatus(context.Context, *entity.Order, bool, bool) error
+		// UpdateOrderStatus(context.Context, *entity.Order, bool, bool) error
 		UpdateOrderPaymentID(context.Context, uuid.UUID, uuid.UUID) error
 	}
 
